1d3d666f5d449015e15dc89aa561d75eeb139a42
舒雄威 <13159166158@163.com>
2020-05-19 16:34:22 +0800

    1.0

---
+++ SourceFiles/GuidePageVC.h
@@ -0,0 +1,13 @@
+//
+//  GuidePageVC.h
+//  iOSApp
+//
+//  Created by Minghui on 16/11/8.
+//  Copyright © 2016年 Tiamo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GuidePageVC : UIViewController
+
+@end
---
+++ SourceFiles/GuidePageVC.m
@@ -0,0 +1,21 @@
+//
+//  GuidePageVC.m
+//  iOSApp
+//
+//  Created by Minghui on 16/11/8.
+//  Copyright © 2016年 Tiamo. All rights reserved.
+//
+
+#import "GuidePageVC.h"
+
+
+@implementation GuidePageVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+        //guide_page1
+    
+}
+
+@end
